0fcdd3fe061001e0fa717ee9b06d17f7f106daae
Rubem Neto <rubemviscard2635@gmail.com>
2025-02-16 18:45:12 +0000

    feat:REPL

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("[SMDB] Starting database server...")
+	for {
+		fmt.Print("[SMDB] ")
+		var input string
+		_, err := fmt.Scanln(&input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if input == ".exit" {
+			fmt.Println("[SMDB] Exiting database server...")
+			return
+		} else {
+			fmt.Println("[SMDB] {ERROR} Command not found {ERROR}")
+		}
+	}
 }
